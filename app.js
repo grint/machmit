480b9995484c40c0ddbaf5304b4345cceadee293
arashmor <arashmor@gmail.com>
2016-04-04 14:26:48 +0200

    Edit activity

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var passport = require('passport');
-var flash    = require('connect-flash');
-var session  = require('express-session');
-var mongoose = require('mongoose');
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/machmit');
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -15,61 +19,53 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// connect to our database
-require('./config/database.js');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use('/public', express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// required for passport
-app.use(session({ secret: '1e8804554c1f41249ae1b522b23f7937' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
-// load our routes and pass in our app and fully configured passport
-require('./routes/routes.js')(app, passport); 
+app.use('/', routes);
+app.use('/users', users);
 
-require('./controllers/passport')(passport); // pass passport methods
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
+// error handlers
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
-// // error handlers
-
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 
 module.exports = app;
