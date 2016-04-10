f500ab36db68b5fe082e784ccc85ce74d45d84dd
arashmor <arashmor@gmail.com>
2016-04-10 11:45:21 +0200

    User muss sich zuerst einloggen

--- routes/routes.js
+++ routes/routes.js
@@ -11,7 +11,106 @@
     // =====================================
     // Activities List =====================
     // =====================================
+	    app.get('/aktlist',isLoggedIn, function(req, res) {
+        // load up the activity model
+        var Activity = require('../models/activity');
+			
+        // query db for all activities
+        Activity.find( function ( err, items, count ) {
+            console.log(items);
+            res.render( 'aktlist', {
+                title : 'Activities',
+                aktlist : items
+            })
+        })
+    });
+	
     require('./activities')(app);
+	
+		/* GET New Aktivitaet page. */
+    app.get('/newaktivity',isLoggedIn, function(req, res) {
+        res.render('newaktivity', { title: 'Add New Aktivitaet' });
+    });
+	
+	
+	/* POST to Add User Service */
+    app.post('/addaktivity',isLoggedIn, function(req, res) {
+		
+		var mongoose = require('mongoose');
+
+        // load up the activity model
+        var Activity = require('../models/activity');
+
+        // create a new activity object
+    	var newAkt = new Activity();
+
+        // Get our form values. These rely on the "name" attributes
+		 newAkt._id = mongoose.Types.ObjectId();
+         newAkt.name = req.body.name;
+    	 newAkt.beschreibung = req.body.beschreibung;
+    	 newAkt.datum = req.body.datum;
+    	 newAkt.uhrzeit = req.body.uhrzeit;
+    	 newAkt.dauer = req.body.dauer;
+    	 newAkt.teilnehmer = req.body.teilnehmer;
+    	 newAkt.adresse.stadt = req.body.stadt;
+    	 newAkt.adresse.strasse = req.body.strasse;
+    	 newAkt.adresse.hausnr = req.body.hausnr;
+    	 newAkt.adresse.plz = req.body.plz; 
+
+        // Submit to the DB
+        newAkt.save(function (err, doc) {
+            if (err) {
+                // If it failed, return error
+                res.send("There was a problem adding the information to the database: " + err);
+            }
+            else {
+                // And forward to success page
+                res.redirect("aktlist");
+            }
+        });
+    });
+	
+	
+	
+	app.get('/edit/:id',isLoggedIn, function(req, res) {
+		var Activity = require('../models/activity');
+		
+		Activity.findById(req.params.id, function (err, activity) {
+            if (err) return handleError(err);
+			res.render( 'edit', {
+                title : 'Edit Aktivität',
+                akt : activity
+            });
+        }); 
+    });	
+	
+	
+	app.post('/update/:id', isLoggedIn,function(req, res) {
+        // load up the activity model
+        var Activity = require('../models/activity');
+
+		// Submit to DB
+        Activity.findById(req.params.id, function (err, activity) {
+            if (err) return handleError(err);
+
+            // Get form values. 
+			activity.name = req.body.name;
+            activity.beschreibung = req.body.beschreibung;
+			activity.datum = req.body.datum;			
+		    activity.uhrzeit = req.body.uhrzeit;
+			activity.dauer = req.body.dauer;
+			activity.teilnehmer = req.body.teilnehmer;
+			activity.adresse.stadt = req.body.stadt;
+			activity.adresse.strasse = req.body.strasse;
+			activity.adresse.hausnr = req.body.hausnr;
+			activity.adresse.plz = req.body.plz;
+
+            activity.save(function (err) {
+                if (err) return handleError(err);
+                res.redirect("/aktlist");
+            });
+        });
+    });	
     
 
     // =====================================
