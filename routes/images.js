235ea31b4f59f3fbc961491412177f2cf0841bc9
grint <grint@inbox.ru>
2016-05-03 12:23:11 +0200

    remove old avatar during uploading a new one

--- routes/images.js
+++ routes/images.js
@@ -1,38 +1,35 @@
  module.exports = function(app) {
 
 	var multer  = require('multer');
+	var fs  = require('fs');
 
-	 var storage = multer.diskStorage({
-	  destination: function (req, file, cb) {
-	    cb(null, 'public/images/uploads/profile/');
-	  },
-	  filename: function (req, file, cb) {
-	    var originalname = file.originalname;
-	    var extension = originalname.split(".");
-	    var username = req.user.name.replace(" ", "").toLowerCase();
-	    filename = username + "_" + Date.now() + '.' + extension[extension.length-1];
-	    cb(null, filename);
-	  },
-		onFileUploadComplete: function (file) {
-			// var fileimage = file.name;
-			console.log("Upload complete");
+	var userPhotosPath = 'public/images/uploads/profile/';
+
+	var storage = multer.diskStorage({
+		destination: function (req, file, cb) {
+			cb(null, userPhotosPath);
+		},
+		filename: function (req, file, cb) {
+			var originalname = file.originalname;
+			var extension = originalname.split(".");
+			var username = req.user.name.replace(" ", "_").toLowerCase();
+			filename = username + "_" + Date.now() + '.' + extension[extension.length-1];
+			cb(null, filename);
 		}
 	});
 
 	var upload = multer({
 		storage: storage,
 		limits: {
-			fieldNameSize: 100,
 			files: 1,
-			fileSize: 1000000 // 1 mb
+			fileSize: 1000000 // 1mb
 		},
 		fileFilter: function (req, file, cb) {
 			if (file.mimetype !== 'image/png'
-            && file.mimetype !== 'image/jpg'
-            && file.mimetype !== 'image/jpeg'
-            && file.mimetype !== 'image/gif') {
-            	console.log('Got file of type', file.mimetype);
-				req.fileValidationError = 'goes wrong on the mimetype';
+			&& file.mimetype !== 'image/jpg'
+			&& file.mimetype !== 'image/jpeg'
+			&& file.mimetype !== 'image/gif') {
+				// console.log('Got file of type', file.mimetype);
 				req.flash('error', 'Only image files are allowed!');
 				return cb(null, false, new Error('Only image files are allowed!'));
 			}
@@ -60,31 +57,30 @@
     	upload(req,res,function(err) {
 	        if(err) {
 	        	req.flash('error', 'Error uploading file.');
-	        } else if(req.fileValidationError) {
-             	req.flash('error', req.fileValidationError);
         	} else {
 	        	req.flash('success', 'The image is successfully uploaded.');
 	        }
 
 	        // console.log(req.file);
 
-	        var filename = req.file.filename;
-
 	        // Submit to DB
 	        User.findById(req.user._id, function (err, user) {
 	            if (err) return handleError(err);
 
-	            user.avatar = filename;
-	            
+	            // remove old avatar
+	            fs.unlink("./" + userPhotosPath + req.user.avatar, function(err){
+					if (err) throw err;
+				});
+
+	            // write new avatar to DB
+	            user.avatar = req.file.filename;
+
 	            user.save(function (err) {
 	                if (err) return handleError(err);
 	                res.redirect("profile");
 	            });
 	        });
-
-	        // res.redirect("/profile");
 	    });
-
     });
 
 }
